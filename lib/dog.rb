bce403521e0c972327e250c480cd5bfde612504d
Precious Humphrey <phumphrey23@gmail.com>
2018-05-18 11:47:26 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,14 @@
 class Dog
-  def name=(dog_name)
-    this_dogs_name = dog_name
+  def name=(dog_name) #writer
+    @this_dogs_name = dog_name
   end
 
-  def name
-    this_dogs_name
+  def name #reader
+    @this_dogs_name
   end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
